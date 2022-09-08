7f4b3098b56147b95d4489f924bf67b711d620bc
Alexander Batrakov <batrakovalex.astro@gmail.com>
2022-09-08 12:30:34 +0200

    some changes

--- src/GravityTools.jl
+++ src/GravityTools.jl
@@ -18,6 +18,7 @@ include("PhysicalFramework.jl")
 #include("DDSTGTest.jl")
 include("PKFramework.jl")
 include("TempoFramework.jl")
+include("Tempo2Framework.jl")
 
 Base.Float64(m::Measurement{Float64}) = Float64(m.val)
 
@@ -33,6 +34,7 @@ export read_DEFGrid, interpolate_DEFMassGrid, interpolate_NS
 export PKFramework, find_initial_masses, check_terms_in_chisqr, find_best_masses, optimize_PK_method, find_masses, obs_params_dataset, ObsParams
 export TempoFramework, GeneralTest, TempoSettings, GridSetttings, calculate!, cut_ddstg_grid!
 export TempoParameter, get_TempoParameter, update_pf_theory!, modify_par_file, run_tempo, get_par_file_work, read_params
+export calculate_t2!
 
 export G_CAV, M_sun, c, d, rad
 export  lvl_1σ, lvl_2σ, lvl_3σ, lvl_4σ, lvl_5σ, lvl_6σ, lvl_7σ, lvl_68CL, lvl_90CL, lvl_95CL, lvl_99CL
