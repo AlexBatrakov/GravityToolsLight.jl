d7d0129387ec94faf3a368cf4d2f443e83d818e3
Alexander Batrakov <batrakovalex.astro@gmail.com>
2023-11-07 12:14:39 +0000

    Build Documentation

--- docs/make.jl
+++ docs/make.jl
@@ -14,8 +14,8 @@ makedocs(
 
 # Optionally, include to push the docs to the gh-pages branch
 deploydocs(
-    root = joinpath(dirname(pathof(GravityToolsLight)), "..", "docs"),
     repo = "github.com/AlexBatrakov/GravityToolsLight.jl",
+    target = "dev"
     deploy_config = Documenter.GitHubActions(),
     branch = "gh-pages"
 )
