c6185c4251bc9cee7be407e88f326c5a1591c626
Alexander Batrakov <batrakovalex.astro@gmail.com>
2023-11-07 10:07:49 +0000

    Build Documentation

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,6 @@ using GravityToolsLight
 makedocs(
     sitename = "GravityToolsLight Documentation",
     modules = [GravityToolsLight],
-    format = Documenter.HTML(),
     pages = [
         "Home" => "index.md",
         "Tempo Versions" => "abstract_tempo.md",
@@ -15,8 +14,8 @@ makedocs(
 # Optionally, include to push the docs to the gh-pages branch
 deploydocs(
     repo = "github.com/AlexBatrakov/GravityToolsLight.jl",
-    branch = "gh-pages",
     deploy_config = Documenter.GitHubActions(),
-    push_preview = true, # set to false when ready for production
+    target = "site", 
+    branch = "gh-pages"
 )
  
